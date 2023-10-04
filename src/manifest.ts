9ddc4158ca9c93c359e61de9a4065e3274eabc56
lopo <lopo@zju.edu.cn>
2023-10-04 23:08:02 +0800

    feat: newtab config

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,35 +1,38 @@
-import { defineManifest } from '@crxjs/vite-plugin'
+import {defineManifest} from '@crxjs/vite-plugin'
 
 export default defineManifest({
-  name: 'create-chrome-ext',
-  description: '',
-  version: '0.0.0',
-  manifest_version: 3,
-  icons: {
-    16: 'img/logo-16.png',
-    32: 'img/logo-34.png',
-    48: 'img/logo-48.png',
-    128: 'img/logo-128.png',
-  },
-  action: {
-    default_popup: 'popup.html',
-    default_icon: 'img/logo-48.png',
-  },
-  background: {
-    service_worker: 'src/background/index.ts',
-    type: 'module',
-  },
-  content_scripts: [
-    {
-      matches: ['http://*/*', 'https://*/*'],
-      js: ['src/content/index.ts'],
+    name: 'amigo',
+    description: '',
+    version: '0.1.0',
+    manifest_version: 3,
+    icons: {
+        16: 'icons/logo@16x16.png',
+        32: 'icons/logo@32x32.png',
+        48: 'icons/logo@48x48.png',
+        128: 'icons/logo@128x128.png',
     },
-  ],
-  web_accessible_resources: [
-    {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
-      matches: [],
+    chrome_url_overrides: {
+        newtab: 'index.html',
     },
-  ],
-  permissions: [],
+    action: {
+        // default_popup: 'popup.html',
+        // default_icon: 'img/logo@48x48.png',
+    },
+    background: {
+        service_worker: 'src/background/index.ts',
+        type: 'module',
+    },
+    content_scripts: [
+        {
+            matches: ['http://*/*', 'https://*/*'],
+            js: ['src/content/index.ts'],
+        },
+    ],
+    web_accessible_resources: [
+        {
+            resources: [],
+            matches: ['<all_urls>'],
+        },
+    ],
+    permissions: [],
 })
