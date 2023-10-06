56251bf44095931b062c179583cb0d9529ce6056
lopo <lopo@zju.edu.cn>
2023-10-06 21:05:04 +0800

    feat: basic prelude

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,7 @@
 import {Logger} from "../utils/logger";
+import {prepareCrypto} from "./crypto";
+import {ChannelImpl} from "./channel";
 
 Logger.BACKGROUND.info('[background] started')
+prepareCrypto()
+ChannelImpl.listen()
