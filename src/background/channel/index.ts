1e950dfaabf6184d0a04e9f8100b1c0d98ebccd6
lopo <lopo@zju.edu.cn>
2023-10-06 20:36:00 +0800

    feat: ChannelImpl

---
+++ src/background/channel/index.ts
@@ -0,0 +1,28 @@
+import {ChannelMessageCommon} from "../../declaration/channel";
+import {cryptoResolver} from "./crypto";
+import {Logger} from "../../utils/logger";
+
+const logger = Logger.BACKGROUND
+
+const channelMessageScheduler = (message: ChannelMessageCommon, _sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) => {
+    const {channel, origin, payload} = message
+
+    switch (channel) {
+        case 'crypto':
+            cryptoResolver(origin, payload, sendResponse)
+            break
+    }
+
+    return true
+}
+
+abstract class ChannelImpl {
+    public static listen() {
+        chrome.runtime.onMessage.addListener(channelMessageScheduler)
+        logger.info('[messenger] listener added')
+    }
+}
+
+export {
+    ChannelImpl
+}
