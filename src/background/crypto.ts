32b65628e13cd3416eea0f63fbdaaae0084cec33
lopo <lopo@zju.edu.cn>
2023-10-06 20:34:16 +0800

    feat: setup crypto

---
+++ src/background/crypto.ts
@@ -0,0 +1,34 @@
+import init, {V0, V2} from "rusty_crypto"
+import {Logger} from "../utils/logger";
+
+const logger = Logger.BACKGROUND;
+
+// TODO: replace key according to your crxId
+const PLUGIN_CRYPTO_KEY = '2Nvi6p8dVGBalBAEVS/alnC12kkxPK4RKfKJRqtmvckcRoilZTqnQdwp3Alc6Omx'
+
+/**
+ * Prepare the crypto module for use. Should be called as early as possible.
+ */
+const prepareCrypto = async () => {
+    logger.verbose('[crypto] fetching')
+    await fetch('/misc/web.wasm')
+        .then(r => r.arrayBuffer())
+        .then(init)
+        .then(_ => {
+            logger.info('[crypto] initialized');
+
+            // TODO: replace key according to your crxId
+            const isOk = V2.register_crx(V0.encode_base64(chrome.runtime.id))
+            logger.info(`[crypto] registered | result = ${isOk}`)
+        })
+        .catch(err => {
+            logger.error(`[crypto] ${err}`)
+        })
+        .finally(() => {
+            logger.verbose('[crypto] finished')
+        })
+}
+
+export {
+    prepareCrypto
+}
